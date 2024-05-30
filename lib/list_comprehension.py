c9541816095a77cfd203aa1b00a7696e7e328183
wathika-eng <wathika02@gmail.com>
2024-05-30 11:39:39 +0300

    fixed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
-    pass
+    if not num_list:
+        return []
+    else:
+        return [num for num in num_list if num % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:
+        return []  # return empty list is empty
+    else:
+        return [s + "!" for s in sentence_list]
